91cae9646b068fee75045c1053bef02edb1b5b02
Maksym Kalani <kalanimaxim@gmail.com>
2025-05-15 15:16:01 +0300

    Add new CLI tools and update package configuration
    Replaced `cowsay` with `neofetch` in home packages for improved utility. Introduced a new `cli` feature with additional tools like `coreutils`, `htop`, and `zip`, and updated imports in `virtual.nix` to include the `cli` configuration.

--- home/maksym/home.nix
+++ home/maksym/home.nix
@@ -23,7 +23,7 @@
   # environment.
   home.packages = with pkgs; [
     cowsay
-    
+    neofetch
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
