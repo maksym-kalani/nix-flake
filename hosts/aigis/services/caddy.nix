ac56335e886fdce4c45b0c8a483ae89d26378e7e
Maksym Kalani <kalanimaxim@gmail.com>
2025-05-30 01:09:06 +0300

    Disable Caddy service in Aigis configuration
    The Caddy service has been disabled by setting `enable` to false. This change might be temporary or intentional depending on the deployment needs, ensuring the service does not run on Aigis.

--- hosts/aigis/services/caddy.nix
+++ hosts/aigis/services/caddy.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   services.caddy = {
-   enable = true;
+   enable = false;
    package = pkgs.caddy.withPlugins {
      plugins = [ "github.com/caddy-dns/namecheap@v0.0.0-20250228023406-ef9fadb67785" ];
      hash = "sha256-VOPxBx0GvgidMXmt2UvVUTIT6yqF7HxeI4FT9+vk+pk=";
