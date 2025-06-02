1ff19bcdaba40906b6207bf4d87fe34d4194e385
Maksym Kalani <kalanimaxim@gmail.com>
2025-06-03 00:11:09 +0300

    Enable automatic system upgrades with flake support.
    This change activates autoUpgrade with flake inputs for system updates. It includes options for logging build processes and updating "nixpkgs". Scheduling is set for 2:00 AM with a randomized delay of up to 45 minutes.

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -53,4 +53,16 @@
       ((lib.filterAttrs (_: lib.isType "flake")) inputs);
     nixPath = [ "/etc/nix/path" ];
   };
+  
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L" # print build logs
+    ];
+    dates = "04:00";
+    randomizedDelaySec = "45min";
+  };
 }
