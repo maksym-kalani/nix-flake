beded126b01e086dcbf5306056919a3d2af5872c
Maksym Kalani <kalanimaxim@gmail.com>
2025-06-25 19:14:32 +0300

    Enable and configure Caddy with external Caddyfile.
    Caddy service is now enabled and configured to use an external Caddyfile located at /mnt/tank/appdata/caddy/Caddyfile. Removed inline virtual host configurations and updated service group to "tankusers" for better integration.

--- hosts/aigis/services/caddy.nix
+++ hosts/aigis/services/caddy.nix
@@ -1,58 +1,14 @@
 { config, pkgs, ... }:
 {
   services.caddy = {
-   enable = false;
+   enable = true;
    package = pkgs.caddy.withPlugins {
      plugins = [ "github.com/caddy-dns/namecheap@v0.0.0-20250228023406-ef9fadb67785" ];
      hash = "sha256-VOPxBx0GvgidMXmt2UvVUTIT6yqF7HxeI4FT9+vk+pk=";
    };
-
-    globalConfig = ''
-    '';
-    
-   virtualHosts = 
-    {
-      "portainer.laufin.xyz" = {
-        extraConfig = ''
-          encode gzip
-          reverse_proxy https://192.168.2.201:9443 {
-            header_up Host {host}
-            header_up X-Real-IP {client_host}
-            header_up X-Forwarded-For {remote_host}
-            transport http {
-              tls
-              tls_insecure_skip_verify
-            }
-          }
-        '';
-      };
-      "dns2.laufin.xyz" = {
-        extraConfig = ''
-          reverse_proxy http://192.168.2.207:5380
-        '';
-      };
-      "todo.laufin.xyz" = {
-        extraConfig = ''
-          reverse_proxy http://192.168.2.201:1337
-        '';
-      };
-      "git.laufin.xyz" = {
-        extraConfig = ''
-          reverse_proxy http://192.168.2.201:3443
-        '';
-      };
-      "matrix-admin.laufin.xyz" = {
-        extraConfig = ''
-          reverse_proxy http://192.168.2.201:7373
-        '';
-      };
-    };
+   configFile = "/mnt/tank/appdata/caddy/Caddyfile";
+   group = "tankusers";
   };
-  
-  systemd.services.caddy.serviceConfig.EnvironmentFile = [
-    config.sops.secrets.namecheap_api_user.path
-    config.sops.secrets.namecheap_api_key.path
-  ];
   
   networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
