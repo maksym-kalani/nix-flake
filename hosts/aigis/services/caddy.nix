4e40d61b8882820c63f544bccf01f30e12949019
maksym <kalanimaxim@gmail.com>
2025-09-05 22:42:20 +0300

    Update Caddy configuration: update plugin hash for namecheap DNS plugin

--- hosts/aigis/services/caddy.nix
+++ hosts/aigis/services/caddy.nix
@@ -4,7 +4,7 @@
    enable = true;
    package = pkgs.caddy.withPlugins {
      plugins = [ "github.com/caddy-dns/namecheap@v0.0.0-20250228023406-ef9fadb67785" ];
-     hash = "sha256-Cl7lS1v0wly0YUhvcwzfqC1DTrdROwFVOPQCynCvA/c=";
+     hash = "sha256-//dLv0ZvU4WVlSTzv7XvYmdBwNM3xYxF+omfPrqkRho=";
    };
    configFile = "/mnt/tank/appdata/caddy/Caddyfile";
    group = "tankusers";
