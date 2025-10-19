ecad7ad9705f99579cfee227085ab2ba76b1f6be
maksym <kalanimaxim@gmail.com>
2025-10-19 22:10:30 +0300

    Update Caddy DNS plugin hash in Aigis configuration

--- hosts/aigis/services/caddy.nix
+++ hosts/aigis/services/caddy.nix
@@ -4,7 +4,7 @@
    enable = true;
    package = pkgs.caddy.withPlugins {
      plugins = [ "github.com/caddy-dns/namecheap@v0.0.0-20250228023406-ef9fadb67785" ];
-     hash = "sha256-wUePK4L1BUfyhCviR9gl8WPTGKfEog5ikRRlTxp95KQ=";
+     hash = "sha256-krmtHa12siY1Lx53EV2qdKATDa9AzfZMTAKUN8G6j4o=";
    };
    configFile = "/mnt/tank/appdata/caddy/Caddyfile";
    group = "tankusers";
