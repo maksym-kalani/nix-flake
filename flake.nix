1d05b4c202057300c42ce1a99bf7cf7abcd54e8d
maksym <kalanimaxim@gmail.com>
2025-11-22 01:19:11 +0200

    Revert "system warning fix"
    This reverts commit ddba03630c1e59ed5837df86382c4c84da81c9fc.

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
       forAllSystems = nixpkgs.lib.genAttrs systems;
     in {
       packages =
-        forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${stdenv.hostPlatform.system});
+        forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
         
       overlays = import ./overlays {
         inherit inputs outputs;
