ddc891d4fd87d281077b1b026336774ff0bfb026
Maksym Kalani <kalanimaxim@gmail.com>
2025-12-27 00:21:20 +0200

    Update lock

--- hosts/aigis/services/caddy.nix
+++ hosts/aigis/services/caddy.nix
@@ -4,7 +4,7 @@
    enable = true;
    package = pkgs.caddy.withPlugins {
      plugins = [ "github.com/caddy-dns/namecheap@v0.0.0-20250228023406-ef9fadb67785" ];
-     hash = "sha256-rT/x3YeO2X5BcnFYw8XoExy0ieHPgh5AMVDYeVQkpJc=";
+     hash = "sha256-6AiQ8kb/1JN/ZG32bJaSYYTgFnGgrfNZ46atr11bPTc=";
    };
    configFile = "/var/lib/caddy/Caddyfile";
    group = "tankusers";
