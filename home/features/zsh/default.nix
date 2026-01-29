ce8ea7e1003a8951e68887ff398376c73902abe3
maksym <kalanimaxim@gmail.com>
2026-01-29 19:56:01 +0200

    update zsh configuration to use XDG-compliant paths and enhance theme with new prompt segment

--- home/features/zsh/default.nix
+++ home/features/zsh/default.nix
@@ -3,7 +3,7 @@
 {
   programs.zsh = {
     enable = true;
-    dotDir = ".config/zsh";
+    dotDir = "${config.xdg.configHome}/zsh";
 
     history = {
       size = 10000;
@@ -64,7 +64,7 @@
       EDITOR = "nvim";
     };
 
-    initExtra = ''
+    initContent = ''
       # FZF key bindings
       source <(fzf --zsh)
 
