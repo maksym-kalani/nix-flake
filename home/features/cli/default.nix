3b9e8b25183c69242ee3e203901e78c04734fed2
maksym <kalanimaxim@gmail.com>
2026-01-30 23:54:15 +0200

    update fastfetch configuration: change logo type to `small` for consistent appearance adjustments

--- home/features/cli/default.nix
+++ home/features/cli/default.nix
@@ -27,7 +27,7 @@
     {
       "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
       "logo": {
-        "source": "nixos",
+        "type": "small",
         "padding": {
           "top": 1
         }
