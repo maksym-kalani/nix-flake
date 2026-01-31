e3977d04672ed98c0caa48fcaa4fc2e17c7f8e2a
maksym <kalanimaxim@gmail.com>
2026-02-01 00:05:24 +0200

    Revert "add SSH configuration for Aigis: enable SSH program and manage keys with SOPS"
    This reverts commit 1b04a76baf246b56e7d9abea878d7d244d6545a5.

--- home/maksym/home.nix
+++ home/maksym/home.nix
@@ -76,16 +76,4 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
-  # SSH configuration
-  programs.ssh = {
-    enable = true;
-    matchBlocks = {
-      "aigis" = {
-        hostname = "192.168.2.50";
-        user = "maksym";
-        identityFile = "~/.ssh/maksym-aigis";
-      };
-    };
-  };
 }
