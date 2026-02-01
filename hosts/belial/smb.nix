f24e535421a04922f1bdd5311f143850049e8b22
Maksym Kalani <kalanimaxim@gmail.com>
2026-02-01 16:24:14 +0200

    update SMB mount paths for Belial

--- hosts/belial/smb.nix
+++ hosts/belial/smb.nix
@@ -11,33 +11,19 @@
   };
 
   # Mount aigis SMB shares
-  fileSystems."/mnt/aigis/users" = {
+  fileSystems."/home/maksym/aigis/users" = {
     device = "//192.168.2.50/users";
     fsType = "cifs";
-    options = [
-      "credentials=${config.sops.secrets.smb_credentials.path}"
-      "uid=1000"
-      "gid=100"
-      "x-systemd.automount"
-      "noauto"
-      "x-systemd.idle-timeout=60"
-      "x-systemd.device-timeout=5s"
-      "x-systemd.mount-timeout=5s"
-    ];
+    options = let
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in ["${automount_opts},credentials=${config.sops.secrets.smb_credentials.path}"];
   };
 
-  fileSystems."/mnt/aigis/media" = {
+  fileSystems."/home/maksym/aigis/media" = {
     device = "//192.168.2.50/media";
     fsType = "cifs";
-    options = [
-      "credentials=${config.sops.secrets.smb_credentials.path}"
-      "uid=1000"
-      "gid=100"
-      "x-systemd.automount"
-      "noauto"
-      "x-systemd.idle-timeout=60"
-      "x-systemd.device-timeout=5s"
-      "x-systemd.mount-timeout=5s"
-    ];
+    options = let
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in ["${automount_opts},credentials=${config.sops.secrets.smb_credentials.path}"];
   };
 }
