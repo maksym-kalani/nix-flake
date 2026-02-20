3bc591d7c7e9febfa2c44f0bd31b9186af87564c
Maksym Kalani <kalanimaxim@gmail.com>
2026-02-20 23:26:22 +0200

    add getty art

--- hosts/belial/shell.nix
+++ hosts/belial/shell.nix
@@ -1,10 +1,42 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
 
   # Required for xdg.portal when using home-manager with useUserPackages
-  environment.pathsToLink = ["/share/applications" "/share/xdg-desktop-portal"];
+  environment.pathsToLink = [
+    "/share/applications"
+    "/share/xdg-desktop-portal"
+  ];
   environment.interactiveShellInit = ''
     alias rebuild='sudo nixos-rebuild switch --flake .#belial'
+  '';
+
+  services.getty.greetingLine = ""; # suppress default
+  services.getty.helpLine = "";
+
+  environment.etc."issue".text = ''
+    \e[H\e[2J\e[3J
+    \e[1;34m          ▗▄▄▄       ▗▄▄▄▄    ▄▄▄▖             \e[1;31m\e[0m
+    \e[1;34m          ▜███▙       ▜███▙  ▟███▛             \e[1;31m\e[0m
+    \e[1;34m           ▜███▙       ▜███▙▟███▛              \e[1;31m\e[0m
+    \e[1;34m            ▜███▙       ▜██████▛               \e[1;31m\e[0m
+    \e[1;34m     ▟█████████████████▙ ▜████▛     ▟▙         \e[1;31m\e[0m
+    \e[1;34m    ▟███████████████████▙ ▜███▙    ▟██▙        \e[1;31m@@@@@@@   @@@@@@@@  @@@       @@@   @@@@@@   @@@\e[0m
+    \e[1;34m           ▄▄▄▄▖           ▜███▙  ▟███▛        \e[1;31m@@@@@@@@  @@@@@@@@  @@@       @@@  @@@@@@@@  @@@\e[0m
+    \e[1;34m          ▟███▛             ▜██▛ ▟███▛         \e[1;31m@@!  @@@  @@!       @@!       @@!  @@!  @@@  @@!\e[0m
+    \e[1;34m         ▟███▛               ▜▛ ▟███▛          \e[1;31m!@   @!@  !@!       !@!       !@!  !@!  @!@  !@!\e[0m
+    \e[1;34m▟███████████▛                  ▟██████████▙    \e[1;31m@!@!@!@   @!!!:!    @!!       !!@  @!@!@!@!  @!!\e[0m
+    \e[1;34m▜██████████▛                  ▟███████████▛    \e[1;31m!!!@!!!!  !!!!!:    !!!       !!!  !!!@!!!!  !!!\e[0m
+    \e[1;34m      ▟███▛ ▟▙               ▟███▛             \e[1;31m!!:  !!!  !!:       !!:       !!:  !!:  !!!  !!:\e[0m
+    \e[1;34m     ▟███▛ ▟██▙             ▟███▛              \e[1;31m:!:  !:!  :!:        :!:      :!:  :!:  !:!   :!:\e[0m
+    \e[1;34m    ▟███▛  ▜███▙           ▝▀▀▀▀               \e[1;31m :: ::::   :: ::::   :: ::::   ::  ::   :::   :: ::::\e[0m
+    \e[1;34m    ▜██▛    ▜███▙ ▜██████████████████▛         \e[1;31m:: : ::   : :: ::   : :: : :  :     :   : :  : :: : :\e[0m
+    \e[1;34m     ▜▛     ▟████▙ ▜████████████████▛          \e[1;31m\e[0m
+    \e[1;34m           ▟██████▙         ▜███▙              \e[1;31m\e[0m
+    \e[1;34m          ▟███▛▜███▙         ▜███▙             \e[1;31m\e[0m
+    \e[1;34m         ▟███▛  ▜███▙         ▜███▙            \e[1;31m\e[0m
+    \e[1;34m         ▝▀▀▀    ▀▀▀▀▘         ▀▀▀▘            \e[1;31m\e[0m
+
   '';
 }
