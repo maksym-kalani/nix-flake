8574d96a617df7639246970ef3622a64ce5cdb85
Maksym Kalani <kalanimaxim@gmail.com>
2026-02-25 21:29:04 +0200

    fix evaluation warning with system being renamed

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,7 @@
-{inputs, ...}: {
+{ inputs, ... }:
+{
   # This one brings our custom packages from the 'pkgs' directory
-  additions = final: _prev: import ../pkgs {pkgs = final;};
+  additions = final: _prev: import ../pkgs { pkgs = final; };
 
   # This one contains whatever you want to overlay
   # You can change versions, add patches, set compilation flags, anything really.
@@ -24,7 +25,7 @@
 
   stable-packages = final: _prev: {
     stable = import inputs.nixpkgs-stable {
-      system = final.system;
+      system = final.stdenv.hostPlatform.system;
       config.allowUnfree = true;
     };
   };
