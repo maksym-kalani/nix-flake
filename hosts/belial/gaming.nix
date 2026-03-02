994ffa42307ab73adf3150bcf76e3197e39a16fa
Maksym Kalani <kalanimaxim@gmail.com>
2026-03-02 23:44:49 +0200

    add gaming env vars

--- hosts/belial/gaming.nix
+++ hosts/belial/gaming.nix
@@ -16,6 +16,20 @@
     dedicatedServer.openFirewall = true;
     extraCompatPackages = [ pkgs.proton-ge-bin ];
     gamescopeSession.enable = true;
+    package = pkgs.steam.override {
+      extraEnv = {
+        # Toggle: R_Shift+F12
+        MANGOHUD = "1";
+        MANGOHUD_CONFIG = "read_cfg,no_display";
+        GAMEMODERUN = "1";
+        AMD_VULKAN_ICD = "RADV";
+        PROTON_ADD_CONFIG = "fsr4";
+        PROTON_LOCAL_SHADER_CACHE = "1";
+        MESA_SHADER_CACHE_MAX_SIZE = "16G";
+        MESA_GLSL_CACHE_MAX_SIZE = "16G";
+        WINEDLLOVERRIDES = "dinput8,dxgi,dsound=n,b";
+      };
+    };
   };
 
   environment.systemPackages = with pkgs; [
