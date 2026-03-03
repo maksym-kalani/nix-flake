8585c78f0572edb7cda78214034df799256f79f8
Maksym Kalani <kalanimaxim@gmail.com>
2026-03-03 23:38:06 +0200

    Update caddy.nix

--- hosts/aigis/infra/caddy.nix
+++ hosts/aigis/infra/caddy.nix
@@ -2,16 +2,20 @@
   config,
   pkgs,
   ...
-}: {
+}:
+{
   services.caddy = {
     enable = true;
     package = pkgs.caddy.withPlugins {
-      plugins = ["github.com/caddy-dns/namecheap@v0.0.0-20250228023406-ef9fadb67785"];
-      hash = "sha256-4yM9mgG084UaUzVNSxAJiCfvF+9JLW92rq7m3qQOIJw=";
+      plugins = [ "github.com/caddy-dns/namecheap@v0.0.0-20250228023406-ef9fadb67785" ];
+      hash = "sha256-96E3z/8BnHqwagmH4odMEDd2YrJm9zLJiQZfaLb1hcI=";
     };
     configFile = "/var/lib/caddy/Caddyfile";
     group = "tankusers";
   };
 
-  networking.firewall.allowedTCPPorts = [80 443];
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
 }
