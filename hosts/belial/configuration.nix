0b686eae0014050b86222986be798025bd44a265
Maksym Kalani <kalanimaxim@gmail.com>
2026-03-05 19:38:16 +0200

    enable nix-ld

--- hosts/belial/configuration.nix
+++ hosts/belial/configuration.nix
@@ -55,6 +55,8 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  programs.nix-ld.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
