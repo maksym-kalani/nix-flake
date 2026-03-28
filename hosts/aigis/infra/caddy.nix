10d8d0fbe020edca513a299dbbd882804d39118d
Maksym Kalani <kalanimaxim@gmail.com>
2026-03-28 14:54:15 +0200

    update caddy hash

--- hosts/aigis/infra/caddy.nix
+++ hosts/aigis/infra/caddy.nix
@@ -8,7 +8,7 @@
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/namecheap@v0.0.0-20250228023406-ef9fadb67785" ];
-      hash = "sha256-96E3z/8BnHqwagmH4odMEDd2YrJm9zLJiQZfaLb1hcI=";
+      hash = "sha256-oizijdZV+1kkR9BPrVmo/uzV4WwMlb5r7uSN1hAANtk=";
     };
     configFile = "/var/lib/caddy/Caddyfile";
     group = "tankusers";
