ed2ee0dd0c67f60cbf21845360b41c5016606185
Maksym Kalani <kalanimaxim@gmail.com>
2026-05-22 19:11:30 +0300

    Update hermes.nix

--- hosts/aigis/services/hermes.nix
+++ hosts/aigis/services/hermes.nix
@@ -1,10 +1,13 @@
 {
   config,
+  pkgs,
   ...
 }:
 {
   services.hermes-agent = {
     enable = true;
+    container.enable = true;
+    container.hostUsers = [ "maksym" ];
     settings.model.default = "openrouter/auto";
     environmentFiles = [ config.sops.secrets."hermes-env".path ];
     addToSystemPackages = true;
