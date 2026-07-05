569cea7bdaf8bb3695f9aef8110e3dfaf7aa5d2b
Maksym Kalani <kalanimaxim@gmail.com>
2026-07-05 14:36:46 +0300

    update flake.lock

--- hosts/aigis/packages.nix
+++ hosts/aigis/packages.nix
@@ -1,7 +1,8 @@
 {
   pkgs,
   ...
-}: {
+}:
+{
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     intel-gpu-tools
@@ -19,5 +20,8 @@
     restic
     openssh
     sysstat
+  ];
+  nixpkgs.config.permittedInsecurePackages = [
+    "pnpm-9.15.9"
   ];
 }
