30ff85a03e67819487a1fb1fd1cc683ccbfa355c
Maksym Kalani <kalanimaxim@gmail.com>
2026-07-09 22:21:50 +0300

    move stirling pdf to nix service

--- hosts/aigis/services/stirling-pdf.nix
+++ hosts/aigis/services/stirling-pdf.nix
@@ -1,55 +1,30 @@
 {
   ...
-}: let
+}:
+let
   ip = "192.168.2.50";
-  appdata = "/var/lib/containers/";
-  cfg = {
-    name = "stirling-pdf";
-
-    image = "frooodle/s-pdf:latest-ultra-lite";
-
-    port = {
-      internal = 8080;
-      external = 7080;
-    };
-
-    extraOptions = [];
-    volumes = [
-      "${appdata}${cfg.name}/config:/configs:rw"
-      "${appdata}${cfg.name}/logs:/logs:rw"
-
-    ];
-    environmentVariables = {
-      DOCKER_ENABLE_SECURITY = "false";
-      SECURITY_ENABLELOGIN = "false";
+  port = 7080;
+  name = "stirling-pdf";
+in
+{
+  services.stirling-pdf = {
+    enable = true;
+    environment = {
+      SERVER_PORT = port;
+      SECURITY_ENABLELOGIN = false;
       SYSTEM_DEFAULTLOCALE = "en-US";
-      UI_APPNAME = "Stirling-PDF-Ultra-lite";
-      UI_HOMEDESCRIPTION = "Demo site for Stirling-PDF-Ultra-lite Latest";
-      UI_APPNAMENAVBAR = "Stirling-PDF-Ultra-lite Latest";
+      SYSTEM_GOOGLEVISIBILITY = true;
       SYSTEM_MAXFILESIZE = "1000";
-      METRICS_ENABLED = "false";
-      SYSTEM_GOOGLEVISIBILITY = "true";
+      UI_APPNAMENAVBAR = "Stirling-PDF-Ultra-lite Latest";
     };
-
-    autoStart = true;
-  };
-in {
-  virtualisation.oci-containers.containers.${cfg.name} = {
-    image = cfg.image;
-    ports = ["${toString cfg.port.external}:${toString cfg.port.internal}"];
-
-    extraOptions = cfg.extraOptions;
-    volumes = cfg.volumes;
-    environment = cfg.environmentVariables;
-    autoStart = cfg.autoStart;
   };
 
-  networking.firewall.allowedTCPPorts = [cfg.port.external];
+  networking.firewall.allowedTCPPorts = [ port ];
 
   services.gatus.settings.endpoints = [
     {
-      name = cfg.name;
-      url = "http://${ip}:${toString cfg.port.external}";
+      name = name;
+      url = "http://${ip}:${toString port}";
       interval = "1m";
       conditions = [
         "[STATUS] == 200"
@@ -59,7 +34,7 @@ in {
           type = "ntfy";
           enabled = true;
           send-on-resolved = true;
-          description = "${cfg.name} health check";
+          description = "${name} health check";
           failure-threshold = 3;
           success-threshold = 1;
         }
