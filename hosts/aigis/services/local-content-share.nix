01bf41a3cd1a3fa4ad6772257604a7fe1138e090
Maksym Kalani <kalanimaxim@gmail.com>
2026-07-09 22:26:27 +0300

    Update local-content-share.nix

--- hosts/aigis/services/local-content-share.nix
+++ hosts/aigis/services/local-content-share.nix
@@ -1,44 +1,23 @@
 {
   ...
-}: let
+}:
+let
   ip = "192.168.2.50";
-  appdata = "/var/lib/containers/";
-  cfg = {
-    name = "local-content-share";
-
-    image = "tanq16/local-content-share:main";
-
-    port = {
-      internal = 8080;
-      external = 8087;
-    };
-
-    extraOptions = [];
-    volumes = [
-      "${appdata}${cfg.name}:/app/data"
-
-    ];
-    environmentVariables = {};
-
-    autoStart = true;
-  };
-in {
-  virtualisation.oci-containers.containers.${cfg.name} = {
-    image = cfg.image;
-    ports = ["${toString cfg.port.external}:${toString cfg.port.internal}"];
-
-    extraOptions = cfg.extraOptions;
-    volumes = cfg.volumes;
-    environment = cfg.environmentVariables;
-    autoStart = cfg.autoStart;
+  port = 8087;
+  name = "local-content-share";
+in
+{
+  services.local-content-share = {
+    enable = true;
+    port = port;
   };
 
-  networking.firewall.allowedTCPPorts = [cfg.port.external];
+  networking.firewall.allowedTCPPorts = [ port ];
 
   services.gatus.settings.endpoints = [
     {
-      name = cfg.name;
-      url = "http://${ip}:${toString cfg.port.external}";
+      name = name;
+      url = "http://${ip}:${toString port}";
       interval = "1m";
       conditions = [
         "[STATUS] == 200"
@@ -48,7 +27,7 @@ in {
           type = "ntfy";
           enabled = true;
           send-on-resolved = true;
-          description = "${cfg.name} health check";
+          description = "${name} health check";
           failure-threshold = 3;
           success-threshold = 1;
         }
